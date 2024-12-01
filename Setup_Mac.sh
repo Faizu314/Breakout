e8486abb4e849e973daf0325f0b494747a45faaf
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2024-12-01 23:24:00 +0500

    Not redownloading/rebuilding cmake if already locally present
    Tested on a machine without cmake or Xcode IDE.

--- Setup_Mac.sh
+++ Setup_Mac.sh
@@ -21,15 +21,19 @@ CmakePath=""
 if command -v cmake &>/dev/null; then
     echo "CMake is already installed: $(cmake --version)"
     CmakePath="cmake"
+elif [ -f "$(pwd)/CMake/bin/cmake" ]; then
+    echo "CMake found in the local directory: $(pwd)/CMake/bin/cmake"
+    CmakePath="$(pwd)/CMake/bin/cmake"
 else
     echo "CMake not found. Proceeding to download... (script will clone inside directory and not install on the machine)"
     git clone https://github.com/Kitware/CMake.git
     cd CMake
     ./bootstrap
+
     NUM_CORES=$(sysctl -n hw.ncpu)
     echo "Detected $NUM_CORES cores. Using them for parallel build."
-
     make -j"$NUM_CORES"
+
     cd ..
     CmakePath="$(pwd)/CMake/bin/cmake"
     echo "CMake has been built locally. Using CMake at: $CmakePath"
@@ -45,20 +49,22 @@ if [ -d "/Applications/Xcode.app" ]; then
 
     "$CmakePath" .. -G "Xcode"
     xcodebuild -configuration Release
+    executable_path="Release/Game.app"
 else
     echo "Xcode IDE not detected. Using Unix Makefiles generator..."
 
     "$CmakePath" .. -G "Unix Makefiles"
     make
+    executable_path="Game.app"
 fi
 
-read -p "Do you want to run the Breakout? You will find the app bundle at /Build/Release/Game.app (Y/N): " user_input
+read -p "Do you want to run the Breakout? You will find the app bundle at $executable_path (Y/N): " user_input
 
 user_input=$(echo "$user_input" | tr '[:upper:]' '[:lower:]')
 
 if [[ "$user_input" == "y" ]]; then
     echo "Running Breakout game..."
-    open Release/Game.app
+    open "$executable_path"
 else
     echo "Exiting. Game not launched."
 fi
